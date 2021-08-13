4b6e41397dc283f9ad0f76a14157a9ce0407ef57
BlueAccords <blueaccords@gmail.com>
2021-08-13 13:45:57 -0700

    added landing page buttons

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { Button } from '../../components/button';
+import { SCREENS } from '../../components/responsive';
 
 const TopSectionContainer = styled.div`
     min-height: 400px;
-    margin-top: 6em;
+    
     ${tw`
         w-full
         max-w-screen-2xl
@@ -15,6 +17,14 @@ const TopSectionContainer = styled.div`
         pl-3
         pr-3
     `}
+
+    @media (min-width: ${SCREENS.sm}) {
+        margin-top: 2em;
+    }
+
+    @media (min-width: ${SCREENS.md}) {
+        margin-top: 4em;
+    }
 `
 
 const LeftContainer = styled.div`
@@ -56,7 +66,7 @@ const Slogan = styled.h1`
 const Description = styled.p`
     ${tw`
         text-xs
-        lg:text-sm
+        lg:text-lg
         xl:text-lg
         sm:max-h-full
         overflow-hidden
@@ -65,6 +75,12 @@ const Description = styled.p`
     `}
 `
 
+const ButtonsContainer = styled.div`
+    ${tw`
+        flex
+        mt-2
+    `}
+`
 
 
 export function TopSection(){
@@ -72,6 +88,10 @@ export function TopSection(){
         <LeftContainer>
             <Slogan>Megaman Battle Network Deck Builder</Slogan>
             <Description>Build, Share, or Modify folders/decks for the GBA game Megaman Battle Network 6</Description>
+            <ButtonsContainer>
+                <Button theme='filled' text='Create New Folder' />
+                <Button theme='outlined' text='Explore Folders' />
+            </ButtonsContainer>
         </LeftContainer>
     </TopSectionContainer>
 }
